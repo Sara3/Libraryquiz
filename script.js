a5ff6ed084e4e931ab9c30a217773b7e49c5e717
Sara3 <sabera.daqiq@yahoo.com>
2016-05-06 10:26:30 -0400

    submit

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
 
-    $(function() {
-        $( "#sortable" ).sortable({ 
-            placeholder: "ui-sortable-placeholder" 
-        });
-    }); 
-
 	function check_sort() {
 		var list=[];
 		var toSort = document.getElementById("sortable").children;
@@ -14,17 +8,24 @@
 			}
 		}
 		Sprint(list);
-	}
+       
+	}   
+$(function() {
+        $( "#sortable" ).sortable({ 
+            placeholder: "ui-sortable-placeholder" 
+        });
+    }); 
+
 		
 	function numOrdA(a, b){ 
 		return (a-b); 
 	}
 		
 	function Sprint(list){
-		if(list.length == 0){
-		document.getElementById("demo").innerHTML = 
-	    "Book list is sorted";
-		
+		if(list.length == 0){  
+            alert("The Books are sorted!!");
+            window.location.href ="index"+num+".html";
+ 
 		}else{
 		list.sort(numOrdA);
 		document.getElementById("demo").innerHTML ="list of books misplaced:"+list.toString();
